4ab130481f8e50e673873f987874e7dc4ee1f807
Palak Totala <75534806+palaktotala@users.noreply.github.com>
2023-04-05 22:17:03 +0530

    Update server2.py
    has bugs

---
+++ tut03/server2.py
@@ -0,0 +1,62 @@
+import socket
+import sys
+
+# import thread module
+from _thread import *
+import threading
+
+print_lock = threading.Lock()
+
+# thread function
+def threaded(c):
+
+     while True:
+          try:
+               equation=c.recv(1024).decode()
+               if equation == "Q" or equation == "q" or equation == "Quit" or equation == "quit" or equation == "quit()":
+                    c.send("Quit".encode())
+                    break
+               else:
+                    print("Client has given this equation:", equation)
+                    result = eval(equation)
+                    c.send(str(result).encode())
+                    print("Sending reply:", result)
+          except (ZeroDivisionError):
+               c.send("ZD".encode())
+          except (ArithmeticError):
+               c.send("ME".encode())
+          except (SyntaxError):
+               c.send("SE".encode())
+          except (NameError):
+               c.send("NE".encode())
+
+     c.close() 			# Close the connection.
+
+
+def Main():
+
+	host = socket.gethostname()                    # Get local machine name
+	port = int(sys.argv[1])
+	s = socket.socket() 	  		 # Create a socket object
+	s.bind((host, port)) 			 # Bind to the port
+	s.listen(5) 			         # Now wait for client connection.
+
+	print("Server has started yay!")
+
+	# a forever loop until client wants to exit
+	while True:
+
+		# establish connection with client
+		c, addr = s.accept()
+
+		# lock acquired by client
+		print_lock.acquire()
+		print('Connected to :', addr[0], ':', addr[1])
+
+		# Start a new thread and return its identifier
+		start_new_thread(threaded, (c,))
+	s.close()
+
+
+if __name__ == '__main__':
+	Main()
