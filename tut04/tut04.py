9bafeebec86e523a6cf41c8ab595c9d0e08f4189
Palak Totala <75534806+palaktotala@users.noreply.github.com>
2023-05-07 23:33:19 +0530

    Update tut04.py

--- tut04/tut04.py
+++ tut04/tut04.py
@@ -1,115 +1,120 @@
-import copy
 import threading
-import queue
 import time
-from datetime import datetime
+from queue import Queue
 
-lock = threading.Lock()
-start_time = datetime.now()
+# Initialize dictionaries and variables
+neighbours_dict = {}
+weights_dict = {}
+queue_dict = {}
+routing_table_dict = {}
+node_map_dict = {}
+value_map_dict = {}
+updated_dict = {}
+improved_dict = {}
+all_nodes_list = []
+num = 0 
+inf = 10000
+done = 0
 
-class Router:
-    def __init__(self, router_id, adj_list, router_queues):
-        self.router_id = router_id
-        self.adj_list = adj_list
-        self.router_queues = router_queues
-        self.routing_table = {}
-        self.changes = {}
-        self.routing_table["itr"] = [router_id, 0]
-        for router in adj_list.keys():
-            self.routing_table[router] = [-1, -1]
-        self.routing_table[router_id] = [router_id, 0]
-        for router in adj_list[router_id]:
-            self.routing_table[router] = [router, adj_list[router_id][router]]
-        for router in adj_list.keys():
-            self.changes[router] = 0
+# Read input from the topology file
+def read_input():
+    file_object = open(r"topology.txt", "r")
+    idx = 0
+    for line in file_object:
+        line = line.split()
+        if line[0] == "EOF":
+            break
+        if idx == 0:
+            global num
+            num = int(line[0])
+        elif idx == 1:
+            cnt = 0
+            # Initialize dictionaries and lists for each router
+            for node in line:
+                all_nodes_list.append(node)
+                neighbours_dict[node] = []
+                routing_table_dict[node] = [inf] * num
+                weights_dict[node] = [inf] * num
+                updated_dict[node] = [0] * num
+                improved_dict[node] = [0] * num
+                queue_dict[node] = Queue()
+                node_map_dict[node] = cnt
+                value_map_dict[cnt] = node
+                routing_table_dict[node][cnt] = 0
+                cnt += 1
+        else:
+            u, v, w = line[0], line[1], int(line[2])
+            # Add neighbours and weights to dictionaries
+            neighbours_dict[u].append((v, w))
+            neighbours_dict[v].append((u, w))
+            weights_dict[u][node_map_dict[v]] = w
+            weights_dict[v][node_map_dict[u]] = w
+            # Initialize routing table with initial weights
+            routing_table_dict[u][node_map_dict[v]] = w
+            routing_table_dict[v][node_map_dict[u]] = w
+            # Add routers to the queue for communication
+            queue_dict[u].put((v, routing_table_dict[v]))
+            queue_dict[v].put((u, routing_table_dict[u]))
+        idx += 1
 
-    def __call__(self):
-        while self.routing_table["itr"][1] < len(self.adj_list):
-            lock.acquire()
-            print("_________________________")
-            print("|\t|\t\t|")
-            s = "| "+self.router_id+"\t| iteration "+str(self.routing_table["itr"][1])+"\t|"
-            print(s)
-            print("|_______|_______________|", "\n|\t|\t|\t|")
-            print("|", end=' ')
-            print("To", "\t|",
-                  "Via", "\t|", "Cost", "\t|")
-            print("|_______|_______|_______|")
-            print("|\t|\t|\t|")
-            for router in self.adj_list:
-                print("|", end=' ')
-                if self.changes[router]:
-                    print("*", end='')
-                print(router, "\t|",
-                      self.routing_table[router][0], "\t|", self.routing_table[router][1], "\t|")
-            print("|_______|_______|_______|")
-            self.changes = {router: 0 for router in self.adj_list}
-            lock.release()
+# Print the routing table for a specific router
+def print_routing_table(router):
+    to_print = f"{router}:"
+    for i in range(num):
+        to_print += f"\t{value_map_dict[i]}, {'*' if improved_dict[router][i] == 1 else ''}{routing_table_dict[router][i]}"
+    print(to_print)
 
-            for router in self.adj_list:
-                if router != self.router_id:
-                    try:
-                        self.router_queues[router].put_nowait(copy.deepcopy(self.routing_table))
-                    except queue.Full:
-                        print(router, "router's queue was full")
+# Update the routing table for a specific router
+def update_router(router):
+    updated_dict[router] = [0] * num
+    updated_dict[router][node_map_dict[router]] = 1
+    while True:
+        print_routing_table(router)
 
-            time.sleep(2)
+        global done
+        done += 1
+        if done == num:
+            done = 0
+            print('\n')
 
-            while not self.router_queues[self.router_id].full():
+        time.sleep(2)
+
+        improved_dict[router] = [0] * num
+
+        cnt = 0
+        mn, mn_node = inf, 0
+        for val in all_nodes_list:
+            if val == router:
                 continue
+            if routing_table_dict[router][cnt] <= mn and updated_dict[router][cnt] == 0:
+                mn = routing_table_dict[router][cnt]
+                mn_node = cnt
+            cnt += 1
+        if mn_node != -1:
+            updated_dict[router][mn_node] = 1
+            for neighbours in neighbours_dict[value_map_dict[mn_node]]:
+                old = routing_table_dict[router][node_map_dict[neighbours[0]]]
+                new = mn + neighbours[1]
+                routing_table_dict[router][node_map_dict[neighbours[0]]] = min(old, new)
+                if new < old:
+                    improved_dict[router][node_map_dict[neighbours[0]]] = 1
+                queue_dict[neighbours[0]].put((router, routing_table_dict[router]))
 
-            while not self.router_queues[self.router_id].empty():
-                shared_table = self.router_queues[self.router_id].get()
-                for router in self.adj_list:
-                    if (self.routing_table[router][1] == -1 and shared_table[router][router][1] != -1):
-                        self.routing_table[router] = [shared_table["itr"][0], shared_table[router][1] + self.adj_list[self.router_id][shared_table["itr"][0]]]
-                        self.changes[router] = 1
-                    if (self.routing_table[router][1] != -1 and shared_table[router][router][1] != -1):
-                        if (self.routing_table[router][1] > shared_table[router][router][1] + self.adj_list[self.router_id][shared_table["itr"][0]]):
-                            self.routing_table[router] = [shared_table["itr"][0], shared_table[router][1] + self.adj_list[self.router_id][shared_table["itr"][0]]]
-                            self.changes[router] = 1
-                del shared_table
-
-            self.routing_table["itr"][1] += 1
-
+# Main function
 def main():
-    list_routers = []
-    adj_list = {}
-    router_queues = {}
+    # Read input and initialize data structures
+    read_input()
 
-    with open("topology.txt", "r") as input_file:
-        num_routers = int(input_file.readline().strip())
-
-        router_names = input_file.readline().strip().split(' ')
-        for router_name in router_names:
-            list_routers.append(router_name)
-            adj_list[router_name] = {}
-
-        for line in input_file:
-            line = line.strip()
-            if line == "EOF":
-                break
-            node1, node2, cost = line.split()
-            adj_list[node1][node2] = int(cost)
-            adj_list[node2][node1] = int(cost)
-
-    for router_name in list_routers:
-        router_queues[router_name] = queue.Queue(num_routers + 1)
-
-    routers = []
-    for router_name in list_routers:
-        routers.append(Router(router_name, adj_list, router_queues))
-
+    # Create threads for each router and start them
     threads = []
-    for router in routers:
-        thread = threading.Thread(target=router)
-        thread.start()
-        threads.append(thread)
+    for node in all_nodes_list:
+        threads.append(threading.Thread(target=update_router, args=(node,)))
+        threads[-1].start()  
 
+    # Wait for all threads to finish
     for thread in threads:
         thread.join()
 
-    print("Program execution time:", datetime.now() - start_time)
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
+
